99317105c4f80b93661163b7cf57e0288156ef54
Chris White <batknight@gmail.com>
2012-05-09 19:21:44 -0700

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :email, :full_name, :location
+  validates :full_name, :email, presence: true
+  validates :email, format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 end
