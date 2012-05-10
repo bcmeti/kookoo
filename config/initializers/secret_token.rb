3faf58ef5cadab7d2859c38768485440cc1df4dc
Chris White <batknight@gmail.com>
2012-05-09 17:35:53 -0700

    create message resource.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kookoo::Application.config.secret_token = 'a05c7dffd6057860037675af984feaa8cd0609e25ab826ba31e12420fe31b03fe4630a62c60dcea4b98ac89c0ecebd89f58b89bb0fde3c1c29ce2a43d8187da6'
